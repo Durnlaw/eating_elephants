acc86a014b642d4264a3e6049bc99f5142c316a2
Durnlaw <durnlaw@hotmail.com>
2025-05-19 16:43:14 -0400

    time to add extra payments

--- start.py
+++ start.py
@@ -4,6 +4,10 @@ import csv
 from datetime import date
 import pandas as pd
 
+# helpful links
+#https://www.experts-exchange.com/articles/1948/A-Guide-to-the-PMT-FV-IPMT-and-PPMT-Functions.html
+#https://money.stackexchange.com/questions/94140/what-is-the-math-used-to-calculate-the-impact-that-overpaying-a-mortgage-has-an
+
 # setup the loan guesses
 debt_file = 'guesses.tsv'
 debt_owners = []
@@ -20,11 +24,12 @@ with open(debt_file,'r') as data:
             debt_owners.append(line[0])
             descrips.append(line[1])
             principles.append(int(line[2]))
-            int_rates.append(round(float(line[3])*100,4))
+            int_rates.append(round(float(line[3]),4))
             payments_left.append(int(line[4]))
             iter +=1
         # print(line[0], line[1], line[2], line[3], line[4])
 
+original_principles = principles
 total_debts = sum(principles)
 total_payments_remaining = sum(payments_left)
 loan_count = len(debt_owners)
@@ -64,10 +69,11 @@ debt_ratios = []
 for each in range(0, loan_count):
     monthly_rates.append(
         round(
-            (int_rates[each]/100/12*principles[each])/(
-            1-((1+(int_rates[each]/100/12))**-payments_left[each])
-        ),2)
-        )
+            # (int_rates[each]/(1-(1+int_rates[each])**-payments_left[each]))*principles[each]
+            (int_rates[each]/12*principles[each]/(
+            1-(1+(int_rates[each]/12))**-payments_left[each])
+        ),2))
+        # )
     full_term_debts.append(round(monthly_rates[each]*payments_left[each],2))
     debt_ratios.append(round((full_term_debts[each]/principles[each]),2))
 
@@ -80,15 +86,25 @@ print(debt_ratios)
 
 principle_paid = []
 interest_paid = []
-current_payment = [0] * loam_count
+current_payment = [0] * loan_count
 # Now get all of them to tick down at the same time
 while total_payments_remaining >0:
     for each in range(0, loan_count):
         if payments_left[each]>0:
-            current_payment += 1
+            current_payment[each] += 1
             total_payments_remaining += -1
             payments_left[each] += -1
-            print("Payments left:", payments_left[each], monthly_rates[each])
+
+            # now determine how much of the payment is interest and principle
+            int_portion = round(principles[each]*(int_rates[each]/12), 2)
+            #IPMT monthly_rates[each]+(1+(int_rates[each]/12))**(current_payment[each]-1)*((principles[each]*int_rates[each]/12)-monthly_rates[each])
+
+            prin_portion = round(monthly_rates[each] - int_portion, 2)
+            principles[each] += -prin_portion
+
+            print(descrips[each], current_payment[each]
+            , payments_left[each], monthly_rates[each]
+            , int_portion, prin_portion, round(principles[each],2))
             
 print(current_payment)
             # print(principles[each], principles[each]-monthly_rates[each])
