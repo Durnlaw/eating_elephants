0a73474555267aaeb8052512e2d1e3ec669ea11e
durnlaw <durnlaw@hotmail.com>
2025-05-25 00:44:41 -0700

    Loans ticking down sequentially

--- start.py
+++ start.py
@@ -9,7 +9,8 @@ import pandas as pd
 #https://money.stackexchange.com/questions/94140/what-is-the-math-used-to-calculate-the-impact-that-overpaying-a-mortgage-has-an
 
 # import the loan guesses
-debt_file = 'guesses.tsv'
+print("Options: 'guesses_by_prin.tsv', 'guesses_by_rate.tsv'")
+debt_file = input()
 debt_owners = []
 descrips = []
 principles = []
@@ -94,57 +95,97 @@ net_inc_prcnt = .015
 group_extra_pay = net_inc_prcnt*sum(incomes)/12
 print("Monthly contribution:", group_extra_pay)
 
-# cycle through all potential payments
-while total_payments_remaining >0:
+# define a function to walk each loan down WITH the and print out where the loan is out
+def loan_group_walkdown(loan_num):
+    # If the standard monthly rate covers what's left of the loan
+    if (std_mnthly_rate[loan_num]) > principles[loan_num]:
+        int_paid[loan_num] += int_portion
+        remaining_loan = int_portion+principles[loan_num]
+
+        principles[loan_num] = 0
+        print(descrips[loan_num]
+        , current_period[loan_num], payments_left[loan_num]
+        , round(remaining_loan,2), '0'
+        , int_portion
+        , round(principles[loan_num],2), int_paid[loan_num])
+    # If the std plus group extra covers what's left of the loan
+    elif (group_extra_pay+std_mnthly_rate[loan_num]) > principles[loan_num]:
+        int_paid[loan_num] += int_portion
+        remaining_loan = int_portion+principles[loan_num]
+        group_finisher = (group_extra_pay+std_mnthly_rate[loan_num])-principles[loan_num]
+
+        principles[loan_num] = 0
+        print(descrips[loan_num]
+        , current_period[loan_num], payments_left[loan_num]
+        , round(remaining_loan,2), group_finisher
+        , int_portion
+        , round(principles[loan_num],2), int_paid[loan_num])
+    # Standard payment plus group extra paying a normal payment off
+    else:
+        principles[loan_num] += -(prin_portion+group_extra_pay)
+        int_paid[loan_num] += int_portion
+
+        print(descrips[loan_num]
+        , current_period[loan_num], payments_left[loan_num]
+        , std_mnthly_rate[loan_num], group_extra_pay
+        , int_portion
+        , round(principles[loan_num],2), int_paid[loan_num])
+
+# define a function to walk each loan down and print out where the loan is out
+def loan_walkdown(loan_num):
+    # If the standard monthly rate covers what's left of the loan
+    if (std_mnthly_rate[loan_num]) > principles[loan_num]:
+        int_paid[loan_num] += int_portion
+        remaining_loan = int_portion+principles[loan_num]
+
+        principles[loan_num] = 0
+        print(descrips[loan_num]
+        , current_period[loan_num], payments_left[loan_num]
+        , round(remaining_loan,2), '0'
+        , int_portion
+        , round(principles[loan_num],2), int_paid[loan_num])
+    else:
+        principles[loan_num] += -(prin_portion)
+        int_paid[loan_num] += int_portion
+
+        print(descrips[loan_num]
+        , current_period[loan_num], payments_left[loan_num]
+        , std_mnthly_rate[loan_num], 'Grp: 0'
+        , int_portion
+        , round(principles[loan_num],2), int_paid[loan_num])
+
+while total_payments_remaining > 0:
     total_payments_remaining += -1
     # cycle through each loan
     for each in range(0, loan_count):
         # if there is principle left on the loan
         if principles[each]>0:
+            total_payments_remaining += -1
             # if there are still payments left on THIS loan
             if payments_left[each]>0:
                 current_period[each] += 1
                 payments_left[each] += -1
-
                 # now determine how much of the payment is interest and principle
                 int_portion = round(principles[each]*(int_rates[each]/12), 2)
                 prin_portion = round(std_mnthly_rate[each] - int_portion, 2)
-
-                # If the standard monthly rate covers what's left of the loan
-                if (std_mnthly_rate[each]) > principles[each]:
-                    int_paid[each] += int_portion
-                    remaining_loan = int_portion+principles[each]
-
-                    principles[each] = 0
-                    print(descrips[each]
-                    , current_period[each], payments_left[each]
-                    , round(remaining_loan,2), '0'
-                    , int_portion
-                    , round(principles[each],2), int_paid[each])
                 
-                # If the std plus group extra covers what's left of the loan
-                elif (group_extra_pay+std_mnthly_rate[each]) > principles[each]:
-                    int_paid[each] += int_portion
-                    remaining_loan = int_portion+principles[each]
-                    group_finisher = (group_extra_pay+std_mnthly_rate[each])-principles[each]
-
-                    principles[each] = 0
-                    print(descrips[each]
-                    , current_period[each], payments_left[each]
-                    , round(remaining_loan,2), group_finisher
-                    , int_portion
-                    , round(principles[each],2), int_paid[each])
-                
-                # Standard payment plus group extra paying a normal payment off
-                else:
-                    principles[each] += -(prin_portion+group_extra_pay)
-                    int_paid[each] += int_portion
-
-                    print(descrips[each]
-                    , current_period[each], payments_left[each]
-                    , std_mnthly_rate[each], group_extra_pay
-                    , int_portion
-                    , round(principles[each],2), int_paid[each])
+                # next big thing. 
+                # If it's the first loan, process that loan with group_pay
+                if each == 0:
+                    loan_group_walkdown(each)
+                    print(current_period)
+                    print(principles[each])
+                # If it's NOT the first loan, but the first loan is still being paid
+                # process that loan without the group_pay
+                elif current_period[each] == current_period[each-1]:
+                    loan_walkdown(each)
+                    print(current_period)
+                    print(principles[each])
+                # If the first loan is done, then process the next loan with group_pay
+                elif current_period[each] > current_period[each-1]:
+                    loan_group_walkdown(each)
+                    print(current_period)
+                    print(principles[each])
             else:
                 pass
         else:
@@ -158,6 +199,7 @@ for each in range(0, loan_count):
         round(full_term_debts[each]-original_principles[each]-int_paid[each],2))
 
 print(loan_savings)
+print(sum(loan_savings))
 print(current_period)
 
 
@@ -168,25 +210,118 @@ print(current_period)
 
 
 
-# # I'm going to try to do this without using pyloan
-# # This processes each loan through the library calculator to find the worst repayment_to_principal ratio
-# loan_summaries = pd.DataFrame({"loan_amount":[], "total_payment_amount":[], "total_principal_amount"
-#                                :[], "total_interest_amount":[], "residual_loan_balance":[], "repayment_to_principal":[]})
-# for each in range(0,loan_count):
 
-#     # print('--------')
-#     # print(names[each], descrips[each], int_rates[each])
-#     loan = pyloan.Loan(loan_amount=principles[each]
-#                 , interest_rate=int_rates[each]
-#                 , loan_term=payments[each]
-#                 , start_date=current_date
-#                 # , payment_amount=1000
-#                 )
-#     individ_summary_df = pd.DataFrame.from_records([loan.get_loan_summary()],columns=pyloan.Loan_Summary._fields)
-#     loan_summaries = loan_summaries._append(individ_summary_df, ignore_index=True)
-#     # print(loan.get_loan_summary())
-#     # print('------------------------------------------------')
-# # set index
-# loan_summaries.index = descrips
 
-# print(loan_summaries)
+
+
+
+
+
+
+
+
+
+
+
+# # IN ORDER TO MAKE THIS WORK I THINK
+# # you need to move the while statement inside the first for loop. 
+# # Also, record what period the loan is paid off, so you can have an if statement or something
+# # move to the next loan and apply the new group_extra_pay
+
+
+# # cycle through all potential payments
+# while total_payments_remaining >0:
+#     total_payments_remaining += -1
+#     # cycle through each loan
+#     for each in range(0, loan_count):
+#         # if there is principle left on the loan
+#         if principles[each]>0:
+#             # if there are still payments left on THIS loan
+#             if payments_left[each]>0:
+#                 current_period[each] += 1
+#                 payments_left[each] += -1
+
+#                 # now determine how much of the payment is interest and principle
+#                 int_portion = round(principles[each]*(int_rates[each]/12), 2)
+#                 prin_portion = round(std_mnthly_rate[each] - int_portion, 2)
+
+#                 # If the standard monthly rate covers what's left of the loan
+#                 if (std_mnthly_rate[each]) > principles[each]:
+#                     int_paid[each] += int_portion
+#                     remaining_loan = int_portion+principles[each]
+
+#                     principles[each] = 0
+#                     print(descrips[each]
+#                     , current_period[each], payments_left[each]
+#                     , round(remaining_loan,2), '0'
+#                     , int_portion
+#                     , round(principles[each],2), int_paid[each])
+                
+#                 # If the std plus group extra covers what's left of the loan
+#                 elif (group_extra_pay+std_mnthly_rate[each]) > principles[each]:
+#                     int_paid[each] += int_portion
+#                     remaining_loan = int_portion+principles[each]
+#                     group_finisher = (group_extra_pay+std_mnthly_rate[each])-principles[each]
+
+#                     principles[each] = 0
+#                     print(descrips[each]
+#                     , current_period[each], payments_left[each]
+#                     , round(remaining_loan,2), group_finisher
+#                     , int_portion
+#                     , round(principles[each],2), int_paid[each])
+                
+#                 # Standard payment plus group extra paying a normal payment off
+#                 else:
+#                     principles[each] += -(prin_portion+group_extra_pay)
+#                     int_paid[each] += int_portion
+
+#                     print(descrips[each]
+#                     , current_period[each], payments_left[each]
+#                     , std_mnthly_rate[each], group_extra_pay
+#                     , int_portion
+#                     , round(principles[each],2), int_paid[each])
+#             else:
+#                 pass
+#         else:
+#             pass
+
+# # let's handle how much was saved and how long it took to get here
+# loan_savings = []
+
+# for each in range(0, loan_count):
+#     loan_savings.append(
+#         round(full_term_debts[each]-original_principles[each]-int_paid[each],2))
+
+# print(loan_savings)
+# print(current_period)
+
+
+
+
+
+
+
+
+
+# # # I'm going to try to do this without using pyloan
+# # # This processes each loan through the library calculator to find the worst repayment_to_principal ratio
+# # loan_summaries = pd.DataFrame({"loan_amount":[], "total_payment_amount":[], "total_principal_amount"
+# #                                :[], "total_interest_amount":[], "residual_loan_balance":[], "repayment_to_principal":[]})
+# # for each in range(0,loan_count):
+
+# #     # print('--------')
+# #     # print(names[each], descrips[each], int_rates[each])
+# #     loan = pyloan.Loan(loan_amount=principles[each]
+# #                 , interest_rate=int_rates[each]
+# #                 , loan_term=payments[each]
+# #                 , start_date=current_date
+# #                 # , payment_amount=1000
+# #                 )
+# #     individ_summary_df = pd.DataFrame.from_records([loan.get_loan_summary()],columns=pyloan.Loan_Summary._fields)
+# #     loan_summaries = loan_summaries._append(individ_summary_df, ignore_index=True)
+# #     # print(loan.get_loan_summary())
+# #     # print('------------------------------------------------')
+# # # set index
+# # loan_summaries.index = descrips
+
+# # print(loan_summaries)
